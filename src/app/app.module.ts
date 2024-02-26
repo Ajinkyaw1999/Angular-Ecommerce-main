c1afb8ef70f6683105fce7e2001160b8157fb29e
Ajinkya Wagh <142288942+Ajinkyaw1999@users.noreply.github.com>
2024-02-26 16:48:50 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductViewComponent } from './component/product-view/product-view.component';
+import { ProductDetailComponent } from './component/product-detail/product-detail.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartPageComponent } from './component/cart-page/cart-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OrderPageComponent } from './component/order-page/order-page.component';
+import { CardPaymentComponent } from './component/card-payment/card-payment.component';
+import { CardOrderComponent } from './component/card-order/card-order.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { BanerComponent } from './component/baner/baner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductViewComponent,
+    ProductDetailComponent,
+    HeaderComponent,
+    CartPageComponent,
+    OrderPageComponent,
+    CardPaymentComponent,
+    CardOrderComponent,
+    SidebarComponent,
+    BanerComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
